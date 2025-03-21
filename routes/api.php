d2c3a951b0e7aa6cab1b388230d2beff610b86d4
najminaak <najminakinanti2@gmail.com>
2025-03-21 14:56:54 +0700

    register

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,12 @@ Route::middleware(['api'])->group(function () {
     Route::post('/register-organizer', [RegisterController::class, 'organizerRegister'])->name('api.register-organizer');
     Route::post('/organizer-form/{user_id}', [OrganizationController::class, 'store'])->name('api.organizer-form');
     // Endpoint registrasi Entrepreneur
-    Route::post('/register-entrepreneur', [RegisterController::class, 'entrepreneurRegister'])->name('api.register-entrepreneur');
-    Route::post('/mitra-form/{user_id}', [MitraController::class, 'store'])->name('api.mitra-form');
+    Route::post('/register-entrepreneur', [RegisterController::class, 'entrepreneurRegister']);
+    Route::post('/mitra-form/{user_id}', [MitraController::class, 'store']);
     Route::put('/user-form/{user_id}', [UserDataController::class,'updateUserData'])->name('api.user-form');
-    Route::post('password/send-otp', [OTPResetController::class, 'sendOTP']);
-    Route::post('password/verify-otp', [OTPResetController::class, 'verifyOTP']);
-    Route::post('password/reset-with-otp', [OTPResetController::class, 'resetPassword']);
+    Route::post('/password/send-otp', [OTPResetController::class, 'sendOTP']);
+    Route::post('/password/verify-otp', [OTPResetController::class, 'verifyOTP']);
+    Route::post('/password/reset-with-otp', [OTPResetController::class, 'resetPassword']);
 
     Route::middleware(['auth:api', 'scope:entrepreneur'])->group(function () {
         Route::prefix('events')->group(function () {
